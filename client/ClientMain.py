4005a90c7174f7f5048cc29a6e825c03e8413ac9
Paige <39844324+pdare@users.noreply.github.com>
2024-04-04 12:39:50 -0400

    lift formatting
    get_lift now returns a properly formatted dictionary of all key/value pairs

--- client/ClientMain.py
+++ client/ClientMain.py
@@ -1,6 +1,11 @@
 import socket
 import json
 import os
+import re
+from enum import Enum
+
+class FormatType(Enum):
+    GETLIFT = 1
 
 def client_program():
     get_input = input("enter command: ")
@@ -17,9 +22,30 @@ def tuple_to_string(tuple):
         output += item
     return output
 
-def format_output_string(output_str):
-        result = output_str.strip('()')
-        return result
+def format_output_string(format_type, output_str):
+    result = {}
+    match format_type:
+        case FormatType.GETLIFT:
+            date_regex = "\\(.*\\)"
+            no_date_regex = "\\'.*"
+            stripped_output_str = output_str.strip('()')
+            stripped_output_str = stripped_output_str[:-2]
+            date = re.findall(date_regex, stripped_output_str)[0]
+            date = date.replace('(', "")
+            date = date.replace(')', "")
+            date = date.replace(',', "")
+            date = date.replace(" ", "-")
+            result.update({"lift_date": date})
+            removed_date = re.findall(no_date_regex, stripped_output_str)
+            removed_date_str = removed_date[0].replace("'", "")
+            split_params = removed_date_str.split(',')
+            result.update({"workout_name": split_params[0].strip()})
+            result.update({"lift_name": split_params[1].strip()})
+            result.update({"reps_num": int(split_params[2].strip())})
+            result.update({"weight": int(split_params[3].strip())})
+            result.update({"set_num": int(split_params[4].strip())})
+            result.update({"user_id": int(split_params[5].strip())})
+    return result
 
 def send_lifts():
     filepath = os.path.join(os.getcwd(), "Workout 06 01 03-28-2024")
@@ -65,7 +91,7 @@ def get_lift(user_id, date, set_num):
         message = "bye"
 
     client_socket.close()
-    lift_as_dict = format_output_string(data_from_server)
+    lift_as_dict = format_output_string(FormatType.GETLIFT, data_from_server)
     print(lift_as_dict)
 
 if __name__ == "__main__":
